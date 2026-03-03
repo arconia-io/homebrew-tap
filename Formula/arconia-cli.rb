62f00df6e62f83757cee38c4884a091df24ea23e
jreleaserbot <jreleaser@kordamp.org>
2026-03-03 20:53:14 +0000

    arconia-cli v0.10.0

--- Formula/arconia-cli.rb
+++ Formula/arconia-cli.rb
@@ -1,22 +1,22 @@
-# Generated with JReleaser 1.22.0 at 2026-02-19T23:40:06.231709606Z
+# Generated with JReleaser 1.23.0 at 2026-03-03T20:52:54.178461712Z
 
 class ArconiaCli < Formula
   desc "Enhance the developer experience for Spring Boot"
   homepage "https://github.com/arconia-io/arconia-cli"
-  version "0.9.8"
+  version "0.10.0"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.9.8/arconia-cli-0.9.8-linux-aarch64.zip"
-    sha256 "89f1c9c870dd5df85745cba1cf8c1967ba507695ca4efc5aff074ff12fd36090"
+    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.10.0/arconia-cli-0.10.0-linux-aarch64.zip"
+    sha256 "5c64bf10de2b5e4cb0bbf7db1753427c586356ff88362b96e328652b38641b8f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.9.8/arconia-cli-0.9.8-linux-amd64.zip"
-    sha256 "fc8dd9aab96510ba68d14dc8071b4b1f92afb83c906551328da0ca90c0277bfe"
+    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.10.0/arconia-cli-0.10.0-linux-amd64.zip"
+    sha256 "c6abd669d0cd2e14143d0a16b117671c144a04def2c8142f04fbeb5ee98df719"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.9.8/arconia-cli-0.9.8-macos-aarch64.zip"
-    sha256 "12b6cd02ad25b4ce18903b231999feaabde7f428a6d682babd573c60ae213bf9"
+    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.10.0/arconia-cli-0.10.0-macos-aarch64.zip"
+    sha256 "ccb11ff4b32045e01f1e6c4ba2dd6a497c949d9ee744431facbf0f96136620cd"
   end
 
 
@@ -27,6 +27,6 @@ class ArconiaCli < Formula
 
   test do
     output = shell_output("#{bin}/arconia --version")
-    assert_match "0.9.8", output
+    assert_match "0.10.0", output
   end
 end
