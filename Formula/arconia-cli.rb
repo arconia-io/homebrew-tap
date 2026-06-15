c3e3c2d754211ce8f5475af4ecb629275e029cac
jreleaserbot <jreleaser@kordamp.org>
2026-06-15 05:33:01 +0000

    arconia-cli v0.15.4

--- Formula/arconia-cli.rb
+++ Formula/arconia-cli.rb
@@ -1,22 +1,22 @@
-# Generated with JReleaser 1.24.0 at 2026-06-14T21:21:37.86428496Z
+# Generated with JReleaser 1.24.0 at 2026-06-15T05:32:37.053692207Z
 
 class ArconiaCli < Formula
   desc "Enhance the developer experience for Spring Boot"
   homepage "https://github.com/arconia-io/arconia-cli"
-  version "0.15.3"
+  version "0.15.4"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.15.3/arconia-cli-0.15.3-linux-aarch64.zip"
-    sha256 "5d8ffbdc1d33f96c7511e2112733c53ea1be482a632e36fdb8f1d986694cbaa7"
+    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.15.4/arconia-cli-0.15.4-linux-aarch64.zip"
+    sha256 "18e5f663d5f652d3a2bedea3b4dd904819f73ef59aa9c2b2e86a6a500c9c664b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.15.3/arconia-cli-0.15.3-linux-amd64.zip"
-    sha256 "a1b014c207cf9d01797ce0dead24f29e08c62657f2b7afaabf52016cb90b4d9b"
+    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.15.4/arconia-cli-0.15.4-linux-amd64.zip"
+    sha256 "bb60881e03602bf23c475b5a102622b2b264891825885b2f31031d0bca8f20b2"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.15.3/arconia-cli-0.15.3-macos-aarch64.zip"
-    sha256 "228a7a45fe2994e60dab04a548bfdbedc4f0a683472d051b2cb2a48e73fb2f4e"
+    url "https://github.com/arconia-io/arconia-cli/releases/download/v0.15.4/arconia-cli-0.15.4-macos-aarch64.zip"
+    sha256 "74e1735e1f9ad4cd377104315b35ecb98b16233cb0e77b339106588c0cdf1a8c"
   end
 
 
@@ -27,6 +27,6 @@ class ArconiaCli < Formula
 
   test do
     output = shell_output("#{bin}/arconia --version")
-    assert_match "0.15.3", output
+    assert_match "0.15.4", output
   end
 end
